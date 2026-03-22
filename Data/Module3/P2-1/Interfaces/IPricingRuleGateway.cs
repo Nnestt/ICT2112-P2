f62c8c33c01276554d6e5cdcbf9cc3088773921e
bryankoh30 <bryankoh30@gmail.com>
2026-03-22 19:05:12 +0800

    minor update to ipricingrulegateway

--- Data/Module3/P2-1/Interfaces/IPricingRuleGateway.cs
+++ Data/Module3/P2-1/Interfaces/IPricingRuleGateway.cs
@@ -6,7 +6,7 @@ namespace ProRental.Data.Module3.P2_1.Interfaces;
 public interface IPricingRuleGateway
 {
     List<PricingRule> FindActiveRules();
-    List<PricingRule> FindByTransportMode(TransportMode mode);
+    List<PricingRule>    FindByTransportMode(TransportMode mode);
     void Save(PricingRule rule);
     void Update(PricingRule rule);
 }
