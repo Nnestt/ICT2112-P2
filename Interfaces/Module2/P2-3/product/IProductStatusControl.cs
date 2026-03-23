128f7d91692c2415bcf7c45081858fa1c113bcad
Tan Tze Han <tzehanntan@gmail.com>
2026-03-23 17:29:18 +0800

    threshold value

--- Interfaces/Module2/P2-3/product/IProductStatusControl.cs
+++ Interfaces/Module2/P2-3/product/IProductStatusControl.cs
@@ -5,4 +5,5 @@ namespace ProRental.Interfaces.Domain;
 public interface IProductStatusControl
 {
     bool UpdateProductStatus(int productId, ProductStatus productStatus);
+    int GetThresholdQuantityForProduct(int productId);
 }
