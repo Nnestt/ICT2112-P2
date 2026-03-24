971d81d5a6394a8e5ecb5237c8da19bd94aed0d0
memewei <minwei2002@gmail.com>
2026-03-24 12:43:52 +0800

    fix: Remove DB connection string

--- Data/UnitOfWork/AppDbContext.cs
+++ Data/UnitOfWork/AppDbContext.cs
@@ -8,10 +8,6 @@ namespace ProRental.Data.UnitOfWork;
 
 public partial class AppDbContext : DbContext
 {
-    public AppDbContext()
-    {
-    }
-
     public AppDbContext(DbContextOptions<AppDbContext> options)
         : base(options)
     {
@@ -166,10 +162,6 @@ public partial class AppDbContext : DbContext
     public virtual DbSet<Vettingrecord> Vettingrecords { get; set; }
 
     public virtual DbSet<Warehouse> Warehouses { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=pro_rental;Username=devuser;Password=devpassword");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -2813,7 +2805,7 @@ public partial class AppDbContext : DbContext
         {
             entity.HasKey("Userid").HasName("User_pkey");
 
-            entity.ToTable("User");
+            entity.ToTable("User", "public");
 
             // entity.HasIndex("Email", "User_email_key").IsUnique();
             entity.HasIndex("Email").HasDatabaseName("User_email_key").IsUnique();
